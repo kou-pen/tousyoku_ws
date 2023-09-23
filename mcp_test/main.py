e54ce7ef2b1dc8e6442950c8ab3f5bc7384283cb
kou-pen <kou-pen@outlook.jp>
2023-09-23 11:21:47 +0900

    add mcp_test

---
+++ mcp_test/main.py
@@ -0,0 +1,15 @@
+import mcp3208 as mcp
+
+CE = 0
+SPEED = 10000020 # Hz
+VREF = 3.3 # V
+CH = 0
+
+mcp.Setup(CE,SPEED)
+
+while True:
+    data, volt = mcp.ReadData(CE,CH,VREF)
+    print("%d, %d"%(data,volt))
+
+
+
