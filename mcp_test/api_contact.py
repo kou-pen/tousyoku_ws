95dcd59079d4936c028eede6859240b2f6586abd
kou-pen <kou-pen@outlook.jp>
2023-09-23 12:35:04 +0900

    update mct_test/app.py

--- mcp_test/api_contact.py
+++ mcp_test/api_contact.py
@@ -2,9 +2,16 @@ import requests
 import json
 
 def main():
-    URL = 'http://192.168.0.10:8000/status'
-    res = requests.get(URL)
-    print(res.json())
+    GET_URL = 'http://192.168.0.10:8000/status'
+    POST_URL = 'http://192.168.0.10:8000/toggle'
+    res = requests.get(GET_URL)
+    get_json_data = res.json()
+    now_status = get_json_data["status"]    
+    data = {
+        'status': not now_status
+    }
+    
+    requests.post(POST_URL, json.dumps(data))
     
 if __name__ == '__main__':
     main()
--- mcp_test/app.py
+++ mcp_test/app.py
@@ -1,22 +1,41 @@
 import mcp3208 as mcp
+import wiringpi
+
 import time
+
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+
+class bool_data(BaseModel):
+    status: bool
 
 CE = 0
 SPEED = 10000020 # Hz
 VREF = 3.3 # V
 CH = 0
+TH = 6000
+
+LED1 = 19
+
 
 app = FastAPI()
 
 mcp.Setup(CE,SPEED)
+wiringpi.wiringPiSetupGpio()
+
+wiringpi.pinMode(LED1,1)
 
 @app.get("/status")
 def index():
-    data, volt = mcp.ReadData(CE,CH,VREF)
+    data, _ = mcp.ReadData(CE,CH,VREF)
     print(data)
-    return {"status": data > 1000}
+    return {"status": data > TH}
+
+@app.get("/toggle")
+def toggle(data: bool_data):
+    wiringpi.digitalWrite(LED1,data.status)
+    
 
 if __name__ == '__main__':
     uvicorn.run(app,host='0.0.0.0',port=8000)
