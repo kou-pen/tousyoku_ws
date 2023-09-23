93ef8e1e3e1f8f65597f009fb53e7053fa7888e9
kou-pen <kou-pen@outlook.jp>
2023-09-23 12:48:20 +0900

    make api_led class

--- mcp_test/api_contact.py
+++ mcp_test/api_contact.py
@@ -1,18 +1,31 @@
 import requests
 import json
 
+class api_led():
+    def __init__(self,GET,POST):
+        self.GET_URL = GET
+        self.POST_URL = POST
+    
+    def get_now_status(self):
+        res = requests.get(self.GET_URL)
+        json_data = res.json()
+        return json_data["status"]
+    
+    def post_led_status(self,status):
+        data = {
+            'status': not status
+        }
+        requests.post(self.POST_URL,json.dumps(data))
+        
+    def toggle_led(self):
+        self.post_led_status(self.get_now_status)
+
 def main():
     GET_URL = 'http://192.168.0.10:8000/status'
     POST_URL = 'http://192.168.0.10:8000/toggle'
-    res = requests.get(GET_URL)
-    get_json_data = res.json()
-    now_status = get_json_data["status"]    
-    print(now_status)
-    data = {
-        'status': not now_status
-    }
-    
-    requests.post(POST_URL, json.dumps(data))
+    use_api = api_led(GET_URL,POST_URL)
+    use_api.toggle_led()
+
     
 if __name__ == '__main__':
     main()
